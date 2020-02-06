62c5464767e07ec622c4824e3c9d913f60a62db4
Fabi <fafi@Fabis-MBP.attlocal.net>
2020-02-05 23:02:08 -0600

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,59 @@ class App extends React.Component {
     }
   }
 
+  onChangeType = (pet) => {
+    this.setState({
+      filters: {
+        type: pet
+      }
+    })
+  }
+  findPets = () => {
+    if(this.state.filters.type === 'all'){
+      fetch('/api/pets')
+      .then((res) => res.json())
+      .then((res) => {
+        this.setState({pets: res})
+      })
+    }
+    if(this.state.filters.type === 'cat')
+      fetch('/api/pets?type=cat')
+      .then((res) => res.json())
+      .then((res) => {
+        this.setState({pets: res})
+      })
+    if(this.state.filters.type === 'dog')
+      fetch('/api/pets?type=dog')
+      .then((res) => res.json())
+      .then((res) => {
+        this.setState({pets: res})
+      })
+    if(this.state.filters.type === 'micropig')
+      fetch('/api/pets?type=micropig')
+      .then((res) => res.json())
+      .then((res) => {
+        this.setState({pets: res})
+      })
+  }
+
+ 
+
+  adoptPet = (id) => {
+    const modifiedPet = this.state.pets.map(pet => {
+      if(pet.id === id){
+        let modifiedPet = pet
+        modifiedPet.isAdopted = true
+        return modifiedPet
+      } else {
+        return pet
+      }
+    })
+    this.setState({ 
+    pets: modifiedPet
+     
+    })
+  }
+
   render() {
     return (
       <div className="ui container">
@@ -24,10 +77,10 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters onChangeType={this.onChangeType} onFindPetsClick={this.findPets}/>
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={this.state.pets} onAdoptPet={this.adoptPet}/>
             </div>
           </div>
         </div>
